96cc5706e787bf6ab9bb1516426e3e63abd91cab
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2026-03-01 09:19:46 +0100

    build(storybook): setup storybook

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,28 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    target: "es2021",
-    include: /\.[jt]sx?$/,
-    exclude: [],
-    tsconfigRaw: {
-      compilerOptions: {
-        experimentalDecorators: false,
-        useDefineForClassFields: true,
-      },
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2021",
-    },
-  },
-  build: {
-    target: "es2021",
-  },
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
+	esbuild: {
+		target: "es2021",
+		include: /\.[jt]sx?$/,
+		exclude: [],
+		tsconfigRaw: {
+			compilerOptions: {
+				experimentalDecorators: false,
+				useDefineForClassFields: true,
+			},
+		},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "es2021",
+		},
+	},
+	build: {
+		target: "es2021",
+	},
+	server: {
+		fs: {
+			allow: [".."],
+		},
+	},
 });
