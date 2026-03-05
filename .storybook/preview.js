46fc84d06f71a7ec5ed151dae3c8d469feec767c
Alarion <alarion[bot]@users.noreply.github.com>
2026-03-05 06:32:02 +0000

    feat: infra install and base configuration of playwright  cucumber (#17)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,21 +2,21 @@ import "@awesome.me/webawesome/dist/styles/webawesome.css";
 
 /** @type { import('@storybook/web-components-vite').Preview } */
 const preview = {
-	parameters: {
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
 
-		a11y: {
-			// 'todo' - show a11y violations in the test UI only
-			// 'error' - fail CI on a11y violations
-			// 'off' - skip a11y checks entirely
-			test: "todo",
-		},
-	},
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
 };
 
 export default preview;
